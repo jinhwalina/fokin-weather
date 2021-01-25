ba99465524db562210107ac1b93d290a8f4b157f
jinhwa <slzp0119@naver.com>
2021-01-25 20:43:41 +0900

    1-0 flex

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello Lina!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}></View>
     </View>
   );
 }
@@ -14,13 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: 'white',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   
   },
-  text:{
-    fontSize: 40,
-    color: 'white'
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 8,
+    backgroundColor: "blue"
   }
 });
